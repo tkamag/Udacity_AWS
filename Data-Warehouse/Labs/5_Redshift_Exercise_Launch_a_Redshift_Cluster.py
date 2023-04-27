7307d92212f738ee950f14bcb14b843821401716
Thierry K <thierry.kamagne@gmail.com>
2023-04-27 23:54:11 +0000

    Update

--- Data-Warehouse/Labs/5_Redshift_Exercise_Launch_a_Redshift_Cluster.py
+++ Data-Warehouse/Labs/5_Redshift_Exercise_Launch_a_Redshift_Cluster.py
@@ -51,7 +51,7 @@ response = redshift.create_cluster(
             ClusterIdentifier = DWH_CLUSTER_IDENTIFIER,  
             MasterUsername= DWH_DB_USER,                    
             MasterUserPassword = DWH_DB_PASSWORD, 
-            ClusterSubnetGroupName = CLUSTER_SUNET_GROUP_NAME    
+            ClusterSubnetGroupName = CLUSTER_SUNET_GROUP_NAME,    
             
             # add parameter for role (to allow s3 access)
             IamRoles=['arn:aws:iam::128660232396:role/myRedshiftRole'],
